7b17723493c715806e7afff91e989b49ae88e658
Piotr Hałas <piohal@boozt.com>
2023-11-26 13:32:20 +0100

    Remove PHP-based caching, switch to HTML with SSI
    Removed caching functions from PHP files and substituted them with HTML equivalents using Server Side Includes (SSI) to increase readability and performance. In addition, refactored codebase, replacing PHP include_once statements with SSI includes where possible. Moved JavaScript functions to separate _js.html file for cleaner organization. All these changes are aimed at enhancing the modularity of the project while also simplifying caching processes.

--- www/time.php
+++ www/time.php
@@ -1,5 +1,17 @@
 <?php
 declare(strict_types=1);
-include_once "cache.php";
+function cache_nginx(string $data, int $ttl = 3600): string
+{
+    $key = $_SERVER['HTTP_X_MEMCACHED_KEY'] ?? null;
+    if ($ttl <= 0 || !is_string($key)) {
+        return $data;
+    }
+
+    $mem = new Memcached();
+    $mem->addServer("127.0.0.1", 11211);
+    $mem->set($key, $data, time() + $ttl);
+
+    return $data;
+}
 
 echo cache_nginx((new DateTime())->format('Y-m-d H:i:s'), (int)($_GET['time'] ?? 0));
